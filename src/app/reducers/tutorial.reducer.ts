1c123d734686372147d14501085222234ffd2776
nvanderperren <ik.ben.nas@gmail.com>
2019-01-07 22:47:15 +0100

    implementation of ngrx store

---
+++ src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import * as TutorialActions from './../actions/tutorial.actions';
+
+const initialState: Tutorial = {
+  name: 'Initial Tutorial',
+  url: 'http://google.com'
+};
+
+export function reducer(state: Tutorial[] = [initialState],
+  action: TutorialActions.Actions) {
+
+  switch (action.type) {
+    case TutorialActions.ADD_TUTORIAL:
+      return [...state, action.payload];
+    case TutorialActions.REMOVE_TUTORIAL:
+      const index = action.payload;
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+    default:
+      return state;
+  }
+
+}
